935195640d0f8489f21f1e5ed2cc97a50e014532
Ignacio Rodríguez <rocha.iarch@gmail.com>
2023-06-21 00:56:11 -0400

    CHG: haciendo ajustes a la automatización del script init.sh

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,16 @@ FROM nginx
 # Pasamos como argumento la variable BACKEND_URL (Pre-definida como variable de ambiente)
 # Para definirla podemos usar export BACKEND_URL="http://ejemplo.cl:80"
 ## OJO: usando el archivo init.sh podemos pasar el valor de variable con la opción -e en caso de que nos sea más cómodo.
-ENV BACKEND_URL="$(echo $BACKEND_URL)"
+ENV backend_url="$(echo $BACKEND_URL)"
+ARG backend_url=${backend_url}
 
 # Usando el archivo nginx.conf que armamos, lo copiamos para pasar las configuraciones al contenedor
 COPY nginx.conf /etc/nginx/nginx.conf
+
+# Copiamos los certificados autofirmados
+#### OJO, generar usando el script generate_certificate.sh y completar con los datos que se van pidiendo ###
+COPY cert.pem /etc/nginx/ssl/cert.pem
+COPY key.pem /etc/nginx/ssl/key.pem
 
 # Exponemos los puertos 80 y 443 para habilitar que el server NGINX responda
 EXPOSE 80
--- init.sh
+++ init.sh
@@ -1,8 +1,21 @@
 #! /bin/bash
 
 ## OJO, se recomienda encarecidamente que generemos el certificado autofirmado previo a la ejecución de la construcción y puesta en marcha del contenedor.
-$(pwd)./generate_certificate.sh
+if [ -f cert.pem ] || [ -f key.pem ]; then
+  echo "Archivos de certificado autofirmado corroborados, procederemos con la creación y puesta en marcha de la imagen y contenedor"
+else
+  echo "Se deben crear manualmente los archivos cert.pem y key.pem, para ello puedes usar el script generate_certificate.sh"
+  $(pwd)/./generate_certificate.sh
+fi
 
 # Este script se puede usar para automatizar la creación de la imagen Docker para NGINX, y posteriormente dejar levantado el contenedor con las especificaciones de puertos.
 docker build -t iarch_nginx_reverse_proxy .
-docker run -p 80:80 -p 443:443 -e "BACKEND_URL=http://localhost:80" -d iarch_nginx_reverse_proxy
+
+if [ -n $BACKEND_URL ]; then
+    echo "El valor de la variable backend_url está vacío, por lo que se usará el valor por defecto: http://localhost"
+    export BACKEND_URL="http://localhost"
+    export backend_url="http://localhost"
+fi
+
+#docker run -p 80:80 -p 443:443 -e "backend_url='http://localhost'" -d iarch_nginx_reverse_proxy
+docker run -d iarch_nginx_reverse_proxy
